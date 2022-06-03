aabf9cfb2a5a70173677214b9eee97ec63484ad4
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2022-06-03 17:48:35 +0430

    add test for abstract factory

---
+++ src/main/java/AbstracFactory/Main.java
@@ -0,0 +1,7 @@
+package AbstracFactory;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/AbstracFactory/MainTest.java
@@ -0,0 +1,18 @@
+package AbstracFactory;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class MainTest {
+    @Test
+    @DisplayName("check abstract factory")
+    void checkAbstractFactory() {
+        GardenCreator iranianGardenCreator = new IranianGardenCreator();
+        GardenCreator japaneseGardenCreator = new JapaneseGardenCreator();
+        assertAll(() -> assertEquals("J1", japaneseGardenCreator.createTree().getName()), () -> assertEquals("G1", japaneseGardenCreator.createFlower().getName()), () -> assertEquals("CHENAR", iranianGardenCreator.createTree().getName()), () -> assertEquals("KHATMI", iranianGardenCreator.createFlower().getName()));
+    }
+}
